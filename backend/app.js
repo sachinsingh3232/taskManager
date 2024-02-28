5a6c56a50a0aff38ddbdc5f11306aafc69f6b9dd
SACHIN SINGH <200104049@hbtu.ac.in>
2024-02-29 00:46:48 +0530

    first commit

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const path = require("path");
 const cors = require("cors");
 require("dotenv").config();
 const authRoutes = require("./routes/authRoutes");
@@ -21,10 +20,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/profile", profileRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.resolve(__dirname, "../frontend/build")));
-  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../frontend/build/index.html")));
-}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
